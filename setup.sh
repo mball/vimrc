70462877411c384252e708b31d13a364db731c38
Matt Ball <matt@salesrabbit.com>
2017-06-06 10:29:53 -0600

    Adding setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+dir=`pwd`
+
+# Set up submodules
+git submodule init
+git submodule update
+
+# Symlink vimrc & vim into your home directory (i.e. ~/.vimrc and ~/.vim)
+cd
+ln -s $dir/vimrc .vimrc
+ln -s $dir/vim .vim
+
+# Install plugins
+vim +PluginInstall +qall
+
